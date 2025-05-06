f209de7ba38eaec117773689b1ddc6b27300b993
Matheusctt1Vent <matheuscosta@vent.digital>
2025-05-06 14:28:01 -0300

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from PIL import Image
+
+icone = Image.open("imagens/icone.png")
+
+st.set_page_config(
+    page_title='Fundação Itaú',
+    page_icon=icone,  # Passa o objeto de imagem diretamente
+    layout="wide",
+    initial_sidebar_state='auto',
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    },
+    theme={
+        "primaryColor": "#F63366",  # Cor principal (rosa claro)
+        "backgroundColor": "#ffffff",  # Cor de fundo (branco)
+        "secondaryBackgroundColor": "#F0F2F6",  # Cor de fundo da sidebar (cinza claro)
+        "textColor": "#000000",  # Cor do texto (preto)
+        "font": "sans serif",  # Tipo de fonte
+    }
+)
+
+
+
+pages = {
+    "Navegação": [
+        st.Page("organizacoes.py", title="Organizações", icon=":material/corporate_fare:"),
+        st.Page("perfis.py", title="Meus Perfis", icon=":material/sell:"),
+        st.Page("usuarios.py", title="Usuários", icon=":material/group:")
+    ]
+}
+pg = st.navigation(pages)
+pg.run()
+
+st.logo('.\\imagens\\Logo.png',size='medium',icon_image='.\\imagens\\Logo.png')
+
+with st.sidebar.popover('Matheus', icon=":material/person:", use_container_width=False):
+    col1, col2 = st.columns([1.2, 1])  # coluna da esquerda um pouco maior
+
+    with col1:
+        st.markdown("<div style='padding-right:0px;'><h4>MATHEUS COSTA DA TRINDADE</h4></div>", unsafe_allow_html=True)
+
+    with col2:
+        st.markdown("""
+            <div style='border-left: 2px solid #ccc; padding-left: 10px;'>
+                <p style='margin-bottom: 0;'>Account</p>
+                <p style='margin-top: 4px;'>Log out</p>
+            </div>
+        """, unsafe_allow_html=True)
---
+++ usuarios.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+# Dados simulados
+data = {
+    "E-mail": ["email@email.com"] * 15,
+    "Nome Completo": ["Matheus", "Aline Oliveira Rocha", "Barbara Almeida"] * 5,
+    "Último login": ["03/04/2025"] * 15,
+    "Data criação": ["03/04/2025"] * 15,
+    "Data conf.": ["03/04/2025"] * 15,
+    "Perfil": ["Itaú Cultural"] * 15,
+}
+df = pd.DataFrame(data)
+
+
+st.title('Usuários')
+col1, col2 = st.columns([4, 1])
+with col1:
+    st.text_input('',placeholder="Pesquisar")
+with col2:
+    Page_cliente = st.selectbox(
+    'Perfis', ['Perfil1', 'Perfil2', 'Perfil3'], 0)
+
+st.dataframe(df, hide_index=True)
