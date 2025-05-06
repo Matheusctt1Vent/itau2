0df4af836bd6f142d8075648257358a3b17eed29
Matheusctt1Vent <matheuscosta@vent.digital>
2025-05-06 14:41:27 -0300

    Update Index.py

--- Index.py
+++ Index.py
@@ -15,7 +15,14 @@ st.set_page_config(
     }
 )
 
-
+hide_streamlit_style = """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 pages = {
     "Navegação": [
