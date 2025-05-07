2ce6c00b508ee1b3c0153616154fdea8c2398a21
Matheusctt1Vent <matheuscosta@vent.digital>
2025-05-07 12:53:40 -0300

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -13,7 +13,7 @@ data = {
 df = pd.DataFrame(data)
 
 
-st.header('Usuários', divider=True)
+st.header('teste', divider=True)
 col1, col2 = st.columns([4, 1])
 with col1:
     st.text_input('',placeholder="Pesquisar")
