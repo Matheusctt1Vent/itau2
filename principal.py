97bb77a1ec58bd5e3af9d2dde2af1883b054ec3b
Matheusctt1Vent <matheuscosta@vent.digital>
2025-05-08 17:12:38 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PIL import Image
 
 def mostrar():
     pages = {
@@ -11,15 +10,24 @@ def mostrar():
     }
     pg = st.navigation(pages)
     pg.run()
-    st.logo('imagens/Logo.png',size='medium',icon_image='imagens/Logo.png')
-    with st.sidebar.popover('Matheus', icon=":material/person:", use_container_width=False):
-        col1, col2 = st.columns([1.2, 1])
+    st.logo('.\\imagens\\Logo.png',size='medium',icon_image='.\\imagens\\Logo.png')
+    
+    with st.sidebar.popover('User', icon=":material/person:", use_container_width=False):
+        col1, col2 = st.columns([1,2])
+        
         with col1:
-            st.markdown("<div style='padding-right:0px;'><h4>MATHEUS COSTA DA TRINDADE</h4></div>", unsafe_allow_html=True)
+            st.image('imagens/user.png',width=150)
+
         with col2:
-            st.markdown("""
-                <div style='border-left: 2px solid #ccc; padding-left: 10px;'>
-                    <p style='margin-bottom: 0;'>Account</p>
-                    <p style='margin-top: 4px;'>Log out</p>
-                </div>
-            """, unsafe_allow_html=True)
+            st.markdown(
+                "<div style='padding-right:0px;padding-top:15hpx;'><h4>MATHEUS COSTA DA TRINDADE</h4></div>",
+                unsafe_allow_html=True
+            )
+
+            st.markdown("</div>", unsafe_allow_html=True)
+      
+            if st.button("Log out", key="logout_button"):
+                st.session_state['autenticado'] = False
+                st.rerun()
+
+            st.markdown("</div>", unsafe_allow_html=True)
